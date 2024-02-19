2e398c7b99ce5551b86ee97feb281e961bcc40a3
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2024-02-19 10:05:23 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,9 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.jetbrains:annotations:24.0.1")
+    compileOnly("org.jetbrains:annotations:24.1.0")
 
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
